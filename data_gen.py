bb01edb08eb98b4c1bf3135ea3bcfcf62ba48068
Ben Ricketts <b.ricketts@sron.nl>
2026-03-12 10:53:31 +0100

    Placed visualisations in functions

--- data_gen.py
+++ data_gen.py
@@ -1,6 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def parameter_visualisation(t,gamma,norm,data):
+    fig = plt.figure(figsize=(12, 12))
+    ax1 = plt.subplot(2, 2, 1)
+    ax1.plot(t, gamma)
+    ax1.set_ylabel("Gamma")
+    ax1.set_xlabel("Time (s)")
+
+    ax2 = plt.subplot(2, 2, 2)
+    ax2.plot(t, norm)
+    ax2.set_ylabel("Norm")
+    ax2.set_xlabel("Time (s)")
+
+    ax3 = plt.subplot(2, 1, 2)
+    pcm = ax3.pcolormesh(t, egrid, np.log10(data), shading="auto")
+    fig.colorbar(pcm, ax=ax3, label="log10(Counts)")
+    ax3.set_xlabel("Time (s)")
+    ax3.set_ylabel("Energy (keV)")
+    ax3.set_title("2D Histogram of Counts")
+    ax3.set_yscale("log")
+    plt.tight_layout()
+    plt.savefig("figures/model_variation.png")
+    plt.close()
+
+def model_spectra_visualisation(egrid, data, model, gamma, norm):
+    plt.figure(figsize=(12, 6))
+    ax1 = plt.subplot(1, 2, 1)
+    for i in data.T:
+        ax1.scatter(egrid, i, color="red", alpha=0.1, s=10, marker="x", zorder=2)
+    ax1.plot(egrid, model*dt, color="blue", ls= "--", zorder=1)
+    ax1.set_xlabel("Energy (keV)")
+    ax1.set_ylabel("Counts")
+    ax1.set_title("Model Spectra")
+    ax1.loglog()
+
+    ax2 = plt.subplot(1, 2, 2)
+    ax2.scatter(egrid, np.sum(data,axis=1), color="red", alpha=0.5, s=10, marker="x", zorder=2)
+    ax2.plot(egrid, model_spectrum(egrid, np.mean(gamma), np.mean(norm))*time_length, color="blue", ls= "--", zorder=1)
+    ax2.set_xlabel("Energy (keV)")
+    ax2.set_ylabel("Counts")
+    ax2.set_title("Total data and averaged model spectrum")
+    ax2.loglog()
+    plt.savefig("figures/model_spectra.png")
+    plt.close()
+
 def gen_noise(time, x, fmin=1e-3, fmax=1e2, nfreq=1e2, beta=1.0, norm=1):
     """Adding some noise à la Timmer & Koenig"""
 
@@ -34,49 +78,9 @@ def generate_data(time_length, dt, egrid, gamma, gamma_scatter, norm):
 time_length = 1000
 dt = 10
 egrid = np.logspace(-1, 2, 100)
-data, model, t, gamma, norm = generate_data(time_length, dt, egrid, 2, 0.25, 1000)
+data, model, t, gamma, norm = generate_data(time_length, dt, egrid, 2, 0.25, 10)
 
-fig = plt.figure(figsize=(12, 12))
-ax1 = plt.subplot(2, 2, 1)
-ax1.plot(t, gamma)
-ax1.set_ylabel("Gamma")
-ax1.set_xlabel("Time (s)")
-
-ax2 = plt.subplot(2, 2, 2)
-ax2.plot(t, norm)
-ax2.set_ylabel("Norm")
-ax2.set_xlabel("Time (s)")
-
-ax3 = plt.subplot(2, 1, 2)
-pcm = ax3.pcolormesh(t, egrid, np.log10(data), shading="auto")
-fig.colorbar(pcm, ax=ax3, label="log10(Counts)")
-ax3.set_xlabel("Time (s)")
-ax3.set_ylabel("Energy (keV)")
-ax3.set_title("2D Histogram of Counts")
-plt.tight_layout()
-plt.savefig("figures/model_variation.png")
-plt.close()
-
-plt.figure(figsize=(12, 6))
-ax1 = plt.subplot(1, 2, 1)
-for i in data.T:
-    ax1.scatter(egrid, i, color="red", alpha=0.1, s=10, marker="x", zorder=2)
-ax1.plot(egrid, model*dt, color="blue", ls= "--", zorder=1)
-ax1.set_xlabel("Energy (keV)")
-ax1.set_ylabel("Counts")
-ax1.set_title("Model Spectra")
-ax1.loglog()
-
-ax2 = plt.subplot(1, 2, 2)
-ax2.scatter(egrid, np.sum(data,axis=1), color="red", alpha=0.5, s=10, marker="x", zorder=2)
-ax2.plot(egrid, model_spectrum(egrid, np.mean(gamma), np.mean(norm))*time_length, color="blue", ls= "--", zorder=1)
-ax2.set_xlabel("Energy (keV)")
-ax2.set_ylabel("Counts")
-ax2.set_title("Total data and averaged model spectrum")
-ax2.loglog()
-plt.savefig("figures/model_spectra.png")
-plt.close()
-
-
+parameter_visualisation(t, gamma, norm, data)
+model_spectra_visualisation(egrid, data, model, gamma, norm)
 
 print("Finished")
